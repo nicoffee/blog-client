9ccd9218d6b96456d65c32c1ff7218cb08b2b821
nicoffee <fedoseevn@gmail.com>
2018-03-25 20:13:34 +0500

    Add redux-thunk middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,14 @@
-import { logger } from "redux-logger";
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "../reducers";
+import { logger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
   const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(logger))
+    composeEnhancers(applyMiddleware(ReduxThunk, logger))
   );
   return store;
 };
