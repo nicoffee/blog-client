eb451d8458c0e54c7f46624de4129e1cbf16886c
nicoffee <fedoseevn@gmail.com>
2018-03-29 09:53:23 +0500

    Add createActions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,19 @@
-import { createAction } from 'redux-actions';
+import { createAction, createActions } from 'redux-actions';
 import axios from 'axios';
+import { api } from '../../config.json';
 
 export const fetchPostsAction = createAction('FETCH_POSTS', posts => posts);
 
+export const postsActions = createActions({
+  FETCH_POSTS_STARTED: () => {},
+  FETCH_POSTS_SUCCESS: posts => posts,
+  FETCH_POSTS_ERROR: error => error
+});
+
+console.log('postsActions', postsActions);
+
 export const fetchPosts = () => dispatch => {
-  return axios.get(`http://localhost:3000/posts`).then(response => {
+  return axios.get(`${api}/posts`).then(response => {
     dispatch(fetchPostsAction(response));
   });
 };
