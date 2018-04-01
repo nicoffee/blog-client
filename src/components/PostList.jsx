e98cf4631c7b1cb95ff0dc3e9cad1f91ad0e59ff
nicoffee <fedoseevn@gmail.com>
2018-04-01 11:33:52 +0500

    Fix

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,11 +3,23 @@
 import * as React from 'react';
 import Post from './Post';
 
-// type Props = {
-//   posts: Array<{ title: string, body: string, id: number }>,
-//   fetchPosts: Function
-// };
+type Props = {
+  posts: Array<{ title: string, body: string, id: number }>,
+  fetchPosts: Function
+};
 
-const PostsList = props => <div>123</div>;
+const PostsList = (props: Props) => (
+  <div>
+    <button onClick={() => props.fetchPosts()}>Fetch posts</button>
+    {props.posts.map(post => (
+      <Post
+        key={post.id}
+        title={post.title}
+        body={post.body}
+        img="https://picsum.photos/600/300/?random"
+      />
+    ))}
+  </div>
+);
 
 export default PostsList;
