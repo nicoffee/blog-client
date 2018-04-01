d661ae3cdb4f56c554a25c77bbce3149d5feb07c
nicoffee <fedoseevn@gmail.com>
2018-04-01 11:25:42 +0500

    Fix flow errors

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,18 +3,11 @@
 import * as React from 'react';
 import Post from './Post';
 
-const PostsList = ({ posts: Array<{title: string, body: string}>, fetchPosts: Function}) => (
-  <div>
-    <button onClick={() => fetchPosts()}>Fetch posts</button>
-    {posts.map(post => (
-      <Post
-        key={post.id}
-        title={post.title}
-        body={post.body}
-        img="https://picsum.photos/600/300/?random"
-      />
-    ))}
-  </div>
-);
+// type Props = {
+//   posts: Array<{ title: string, body: string, id: number }>,
+//   fetchPosts: Function
+// };
+
+const PostsList = props => <div>123</div>;
 
 export default PostsList;
