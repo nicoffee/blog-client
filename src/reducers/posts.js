737b0e27b099ba9d224a13efc20776df01a39851
nicoffee <fedoseevn@gmail.com>
2018-04-18 22:56:28 +0500

    Refactor reducers

---
+++ src/reducers/posts.js
@@ -0,0 +1,16 @@
+import { FETCH_POSTS_REQUEST, FETCH_POSTS_SUCCESS, FETCH_POSTS_FAILURE } from "../constants";
+
+const posts = (state = { isFetching: false, items: [] }, action) => {
+  switch (action.type) {
+    case FETCH_POSTS_REQUEST:
+      return { isFetching: true };
+    case FETCH_POSTS_SUCCESS:
+      return { isFetching: false, items: [...action.payload.data] };
+    case FETCH_POSTS_FAILURE:
+      return { isFetching: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default posts;
