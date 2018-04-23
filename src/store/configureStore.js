2a41b9c1fc83b48ee08f56c7e202ea4b0e170216
nicoffee <fedoseevn@gmail.com>
2018-04-23 23:56:47 +0500

    Configure redux-saga

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,23 @@
-import {logger} from 'redux-logger';
-import ReduxThunk from 'redux-thunk';
+import 'regenerator-runtime/runtime';
 import {createStore, applyMiddleware, compose} from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import ReduxThunk from 'redux-thunk';
+import {logger} from 'redux-logger';
 import rootReducer from '../reducers';
+import mySaga from '../sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
   const store = createStore(
     rootReducer,
-    composeEnhancers(applyMiddleware(ReduxThunk, logger))
+    composeEnhancers(applyMiddleware(ReduxThunk, sagaMiddleware, logger))
   );
+
+  sagaMiddleware.run(mySaga);
+
   return store;
 };
 
