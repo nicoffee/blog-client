f740f6678638cffd17ac10041b560ff4be6769cc
Nikolay Fedoseev <n.fedoseev@xsolla.com>
2018-05-01 13:00:44 +0500

    Refactor actions test

--- src/tests/actions.spec.js
+++ src/tests/actions.spec.js
@@ -1,49 +1,123 @@
-import configureMockStore from 'redux-mock-store';
-import thunk from 'redux-thunk';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import {normalize} from 'normalizr';
+import {postListSchema} from '../actions/schema';
 import * as actions from '../actions';
 import * as types from '../constants';
-import {api_prod} from '../../config.json';
 
-const middlewares = [thunk];
-const mockStore = configureMockStore(middlewares);
-const mockAdapter = new MockAdapter(axios);
+describe('fetch posts actions', () => {
+  it('should create an action to fetch posts', () => {
+    const expectedAction = {
+      type: types.FETCH_POSTS_REQUEST,
+    };
 
-describe('async actions', () => {
-  it('creates FETCH_POSTS_SUCCESS when fetching items has been done', () => {
-    mockAdapter.onGet(`${api_prod}/posts`).reply(200, {
-      posts: [
-        {
-          body: 'Quia et suscipit recusandae consequuntur',
-          id: 1,
-          image: 'https://source.unsplash.com/random',
-          title: 'Hello World',
-        },
-      ],
-    });
+    expect(actions.fetchPostsRequest()).toEqual(expectedAction);
+  });
 
-    const expectedActions = [
-      {type: types.FETCH_POSTS_REQUEST},
+  it('should create an action when fetch posts succeeded', () => {
+    const posts = [
       {
-        type: types.FETCH_POSTS_SUCCESS,
-        payload: {
-          posts: [
-            {
-              body: 'Quia et suscipit recusandae consequuntur',
-              id: 1,
-              image: 'https://source.unsplash.com/random',
-              title: 'Hello World',
-            },
-          ],
-        },
+        body: 'Quia et suscipit recusandae consequuntur',
+        id: 1,
+        image: 'https://source.unsplash.com/random',
+        title: 'Hello World',
       },
     ];
 
-    const store = mockStore({posts: []});
+    const expectedAction = {
+      type: types.FETCH_POSTS_SUCCESS,
+      payload: normalize(posts, postListSchema),
+    };
 
-    return store.dispatch(actions.fetchPosts()).then(() => {
-      expect(store.getActions()).toEqual(expectedActions);
-    });
+    expect(actions.fetchPostsSuccess(posts)).toEqual(expectedAction);
+  });
+
+  it('should create an action when fetch posts failed', () => {
+    const error = new Error();
+
+    const expectedAction = {
+      type: types.FETCH_POSTS_FAILURE,
+      payload: error,
+    };
+
+    expect(actions.fetchPostsError(error)).toEqual(expectedAction);
+  });
+});
+
+describe('fetch post info actions', () => {
+  it('should create an action to fetch post info', () => {
+    const postId = 1;
+
+    const expectedAction = {
+      type: types.POST_INFO_REQUEST,
+      id: postId
+    };
+
+    expect(actions.fetchPostInfoRequest(postId)).toEqual(expectedAction);
+  });
+
+  it('should create an action when fetch post info succeeded', () => {
+    const postInfo = {
+      body: 'Quia et suscipit recusandae consequuntur',
+      id: 1,
+      image: 'https://source.unsplash.com/random',
+      title: 'Hello World',
+    };
+
+    const expectedAction = {
+      type: types.POST_INFO_SUCCESS,
+      payload: postInfo,
+    };
+
+    expect(actions.fetchPostInfoSuccess(postInfo)).toEqual(expectedAction);
+  });
+
+  it('should create an action when fetch post info failed', () => {
+    const error = new Error();
+
+    const expectedAction = {
+      type: types.POST_INFO_FAILURE,
+      payload: error,
+    };
+
+    expect(actions.fetchPostInfoError(error)).toEqual(expectedAction);
+  });
+});
+
+describe('edit post info actions', () => {
+  it('should create an action to edit post info', () => {
+    const postId = 1;
+
+    const expectedAction = {
+      type: types.EDIT_POST_REQUEST,
+      id: postId
+    };
+
+    expect(actions.editPostInfoRequest(postId)).toEqual(expectedAction);
+  });
+
+  it('should create an action when edit post info succeeded', () => {
+    const postInfo = {
+      body: 'Quia et suscipit recusandae consequuntur',
+      id: 1,
+      image: 'https://source.unsplash.com/random',
+      title: 'Hello World',
+    };
+
+    const expectedAction = {
+      type: types.EDIT_POST_SUCCESS,
+      payload: postInfo,
+    };
+
+    expect(actions.editPostInfoSuccess(postInfo)).toEqual(expectedAction);
+  });
+
+  it('should create an action when edit post info failed', () => {
+    const error = new Error();
+
+    const expectedAction = {
+      type: types.EDIT_POST_FAILURE,
+      payload: error,
+    };
+
+    expect(actions.editPostInfoError(error)).toEqual(expectedAction);
   });
 });
