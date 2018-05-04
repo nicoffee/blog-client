e0e0897b3c083c06047385363af34d3351c5531c
nicoffee <fedoseevn@gmail.com>
2018-05-04 07:10:05 +0500

    Fix error handling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,15 +7,9 @@ const StyledError = styled.div`
   flex-direction: column;
 `;
 
-const StyledImg = styled.img`
-  width: 300px;
-  height: 400px;
-`;
-
 const Error = props => (
   <StyledError>
     <h1>{props.errorMessage}</h1>
-    <StyledImg src="https://78.media.tumblr.com/93eca4fa0a39eb9ce946eb65087fd123/tumblr_o8fv8exunr1usi9s5o1_1280.png" />
     <Link to="/">Back to main page</Link>
   </StyledError>
 );
--- src/reducers/posts/index.js
+++ src/reducers/posts/index.js
@@ -6,7 +6,8 @@ export const getPosts = state => {
   const ids = fromList.getIds(state.posts.all);
   return ids.map(id => fromById.getPost(state.posts.byId[id], id));
 };
-export const getIsFetching = state => fromList.getIsFetching(state.posts);
-export const getErrorMessage = state => fromList.getErrorMessage(state.posts);
+export const getIsFetching = state => fromList.getIsFetching(state.posts.all);
+export const getErrorMessage = state =>
+  fromList.getErrorMessage(state.posts.all);
 
 export default combineReducers({all: createList(), byId});
