7c0e825d251809b981484441b9c7a0b7cae4bfde
nicoffee <fedoseevn@gmail.com>
2018-05-06 23:12:08 +0500

    Add theme switcher

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@
 import * as React from 'react';
 import {Link} from 'react-router-dom';
 import styled from 'styled-components';
-import UserBlock from '../containers/UserBlock';
+import UserBlock from './UserBlock';
+import Switcher from './Switcher';
 import {Button} from './Styled';
 
 const StyledHeader = styled.div`
@@ -15,32 +16,56 @@ const StyledHeader = styled.div`
   width: 100%;
   top: 0;
   left: 0;
+
+  > div {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+  }
+`;
+
+const StyledSwitcher = styled.div`
+  justify-content: flex-start;
+
+  label {
+    margin-right: 8px;
+  }
 `;
 
 const StyledLogo = styled.div`
-  grid-column: 2 / 3;
-  display: flex;
-  flex-direction: row;
   justify-content: center;
+
+  h1 {
+    margin: 0;
+  }
 `;
 
 const StyledLink = styled.div`
-  grid-column: 3 / 3;
-  display: flex;
-  flex-direction: row;
   justify-content: flex-end;
-  align-items: center;
-  padding: 30px;
 `;
 
 type Props = {
   onSignInClick: Function,
   openModal: Function,
+  switchTheme: Function,
   user: string,
+  theme: string,
 };
 
 const Header = (props: Props) => (
   <StyledHeader>
+    <StyledSwitcher>
+      <Switcher
+        onChange={() =>
+          props.switchTheme(props.theme === 'dark' ? 'light' : 'dark')
+        }
+      />
+      {props.theme === 'dark' ? (
+        <span>Switch to light theme</span>
+      ) : (
+        <span>Switch to night theme</span>
+      )}
+    </StyledSwitcher>
     <StyledLogo>
       <Link to="/">
         <h1>Blog</h1>
