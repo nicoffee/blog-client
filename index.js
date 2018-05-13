018150c7d82e19acef72796da27190de72cf37be
nicoffee <fedoseevn@gmail.com>
2018-05-13 12:09:09 +0500

    Add routes handler

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+// import React from 'react';
+// import {renderToString} from 'react-dom/server';
+// import App from './src/components/App';
+// import Html from './src/html';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+// app.get('/', (req, res) => {
+//   /**
+//    * renderToString() will take our React app and turn it into a string
+//    * to be inserted into our Html template function.
+//    */
+//   const body = renderToString(<App />);
+//   const title = 'Server side Rendering with Styled Components';
+
+//   res.send(
+//     Html({
+//       body,
+//       title,
+//     })
+//   );
+// });
 
 app.listen(port);
